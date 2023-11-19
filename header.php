c56bc0ebba6d9e92f8700c45e68ec45a0f8d668a
Md Mahbob Alam <emptymahbob@gmail.com>
2023-11-20 05:18:31 +0600

    dashboard button removed

--- header.php
+++ header.php
@@ -29,24 +29,13 @@
                         </a>
                     </li>
 
-                    <?php
-                } else {
-
-                    if (!isset($_SESSION['employeeID'])) {
-                    ?>
-
-                        <li class="nav-item">
-                            <a class="nav-link text-white btn btn-outline-info" href="dashboard.php">Dashboard</a>
-                        </li>
-
-                    <?php
-                    } else {
-                    ?>
-                        <li class="nav-item">
-                            <a class="nav-link text-white btn btn-outline-info" href="login.php">Login</a>
-                        </li>
                 <?php
-                    }
+                } else {
+                ?>
+                    <li class="nav-item">
+                        <a class="nav-link text-white btn btn-outline-info" href="login.php">Login</a>
+                    </li>
+                <?php
                 }
                 ?>
                 <li class="nav-item">
